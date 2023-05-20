e78e8b65c0acf475e53ad2cc1e75b76b8fa296ef
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-05-20 09:10:36 +0300

    test

---
+++ js/send-form.js
@@ -0,0 +1,34 @@
+const arrayUrl = [
+  {
+    inUrl: 'https://newsitenew.netlify.app/',
+    toUrl: 'http://example.com',
+  },
+  {
+    inUrl: 'http://testsait/author/andrei/',
+    toUrl: 'http://google.com',
+  },
+];
+
+function setArray(data) {
+  let dataArray = data;
+
+  if (dataArray.length > 0) {
+    dataArray.forEach((link) => {
+      if (link.inUrl === window.location.href) {
+        if (sessionStorage.getItem(link.inUrl) === null) {
+          sessionStorage.setItem(link.inUrl, 1);
+          window.addEventListener('click', openNewWindow);
+          function openNewWindow() {
+            const href = window.location.href;
+            const newTab = window.open(href, '_blank');
+            window.location.href = link.toUrl;
+
+            window.removeEventListener('click', openNewWindow);
+          }
+        }
+      }
+    });
+  }
+}
+
+setArray(arrayUrl);
